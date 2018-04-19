9041470fe40e51ad7d4b8da91fe7a659998d155c
NatalyaSlepkova <slepkovanatal@mail.ru>
2018-04-19 12:24:56 +0300

    block3 add

--- hw2/src/Block3.hs
+++ hw2/src/Block3.hs
@@ -1,12 +1,96 @@
+{-# LANGUAGE InstanceSigs #-}
 module Block3 where
- 
+
 import           Control.Applicative
-import           Data.List
-import           Data.Maybe
 import           Control.Arrow
 import           Control.Monad
 import           Data.Char
-import           Data.Map            (Map)
-import qualified Data.Map            as Map
+import           Data.List
+import           Data.Map(Map)
+import qualified Data.Map as Map
+import           Data.Maybe
 
-data Parser s a = Parser { runParser :: [s] -> Maybe (a, [s]) }
+newtype Parser s a = Parser { runParser :: [s] -> Maybe (a, [s]) }
+
+instance Functor (Parser s) where
+  fmap :: (a -> b) -> Parser s a -> Parser s b
+  fmap f r = Parser (fmap (first f) . runParser r)
+
+instance Applicative (Parser s) where
+  pure :: x -> Parser s x
+  pure x = Parser $ \s -> Just (x, s)
+  (<*>) :: Parser s (x -> y) -> Parser s x -> Parser s y
+  a1 <*> a2 = Parser (runParser a1 >=> (\ (f, s1) -> runParser a2 s1 >>= 
+      \ (x, s2) -> Just (f x, s2)))
+
+instance Monad (Parser s) where
+  return = pure
+  (>>=) :: Parser s x -> (x -> Parser s y) -> Parser s y
+  p >>= f = Parser (runParser p >=> (\(x, sx) -> runParser (f x) sx))
+
+instance Alternative (Parser s) where
+  empty :: Parser s a
+  empty = Parser (const Nothing)
+  (<|>) :: Parser s a -> Parser s a -> Parser s a
+  (<|>) (Parser a1) (Parser a2) = Parser (\s -> a1 s <|> a2 s)
+
+ok :: Parser s ()
+ok = Parser (\s -> Just ((), s))
+
+eof :: Parser s ()
+eof = Parser $ \s -> case s of
+    [] -> Just ((), s)
+    _  -> Nothing
+
+satisfy :: (s -> Bool) -> Parser s s
+satisfy p = Parser f
+  where
+  f [] = Nothing
+  f (x:xs)
+    | p x = Just (x, xs)
+    | otherwise = Nothing
+
+stream :: Eq s => [s] -> Parser s [s]
+stream x = Parser $ \s -> case x of
+    (a:b) -> runParser (element a) s >>= 
+      \(a, t) -> runParser (stream b) t >>= 
+        \(n, f) -> Just(a : n, f)
+    [] -> Just([], s)
+
+char :: Char -> Parser Char Char
+char c = satisfy (== c)
+
+element :: Eq s => s -> Parser s s
+element x = satisfy (== x)
+
+
+spaces :: Parser Char String
+spaces = many (satisfy isSpace)
+
+integer :: Parser Char Integer
+integer = Parser (fmap (first read) . runParser (some (satisfy isDigit)))
+
+sign :: Num p => Char -> p -> p
+sign s v = if s == '-' then -v else v
+
+onlySignedInteger :: Parser Char Integer
+onlySignedInteger =  fmap sign (element '+' <|> element '-') <*> integer
+
+notOnlySignedInteger :: Parser Char Integer
+notOnlySignedInteger =  fmap sign (element '+' <|> element '-' <|> pure ' ') <*> integer
+
+  
+brackets = (element '(' *> brackets *> element ')') *> brackets <|> ok
+
+spacedInteger :: Parser Char Integer
+spacedInteger = spaces *> notOnlySignedInteger <* spaces
+
+commaSpacedInteger :: Parser Char Integer
+commaSpacedInteger = element ',' *> spacedInteger
+
+num 0 _    = Parser (\s -> Just ([], s))
+num n elem = (:) <$> elem <*> num (n - 1) elem
+
+parseListOfLists :: Parser Char [[Integer]]
+parseListOfLists = let l = spacedInteger >>= \n -> num n commaSpacedInteger in
+    fmap (:) l <*> many (element ',' *> l)
--- hw2/tests/Spec.hs
+++ hw2/tests/Spec.hs
@@ -2,6 +2,8 @@ module Main where
 
 import           Block1
 import           Block2
+import           Block3
+import           Data.Char        (isUpper)
 import           Test.Hspec
 import Test.Tasty.Hspec
 import Test.Tasty
@@ -26,3 +28,40 @@ testEval = do
     it "stringSum" $ do
       stringSum "12 34 56 78 90" `shouldBe` Just 270
       stringSum "12 a3 45" `shouldBe` Nothing
+  describe "Block3 tests..." $ do
+    it "satisfy" $ do
+      runParser (satisfy isUpper) "HASKELL" `shouldBe` Just ('H',"ASKELL")
+      runParser (satisfy isUpper) "haskell" `shouldBe` Nothing
+    it "ok" $
+      runParser ok "Haskell" `shouldBe` Just ((),"Haskell")
+    it "eof" $ do
+      runParser eof "Haskell" `shouldBe` Nothing
+      runParser eof "" `shouldBe` Just ((),"")
+    it "element" $ do
+      runParser (element 'c') "Haskell" `shouldBe` Nothing
+      runParser (element 'H') "Haskell" `shouldBe` Just ('H',"askell")
+    it "stream" $ do
+      runParser (stream "c") "Haskell" `shouldBe` Nothing
+      runParser (stream "H") "Haskell" `shouldBe` Just ("H","askell")
+      runParser (stream "Haskel") "Haskell" `shouldBe` Just ("Haskel","l")
+    it "spaces" $ do
+      runParser spaces "Haskell " `shouldBe` Just ("","Haskell ")
+      runParser spaces " Haskell" `shouldBe` Just (" ", "Haskell")
+    it "integer" $ do
+      runParser integer "Haskell" `shouldBe` Nothing
+      runParser integer "4913" `shouldBe` Just (4913,"")
+    it "brackets" $ do
+      runParser brackets ")((" `shouldBe` Just((), ")((")
+      runParser brackets "()()()()((()()))" `shouldBe` Just((), "")
+    it "onlySignedInteger" $ do
+      runParser onlySignedInteger "+Haskell" `shouldBe` Nothing
+      runParser onlySignedInteger "4913" `shouldBe` Nothing
+      runParser onlySignedInteger "+4913" `shouldBe` Just (4913,"")
+      runParser onlySignedInteger "-4913" `shouldBe` Just (-4913,"")
+    it "notOnlySignedInteger" $ do
+      runParser notOnlySignedInteger "Haskell" `shouldBe` Nothing
+      runParser notOnlySignedInteger "+4913" `shouldBe` Just (4913,"")
+      runParser notOnlySignedInteger "4913" `shouldBe` Just (4913,"")
+      runParser notOnlySignedInteger "-4913" `shouldBe` Just (-4913,"")
+    it "parseListOfLists" $
+        runParser parseListOfLists "1, +2,  3, 4, 5, 6,  1, -4913" `shouldBe` Just ([[2], [4, 5, 6], [-4913]], "")
